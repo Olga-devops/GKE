8519a034358d1d8661c8e700ec56e735c776e28b
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-09-02 16:04:30 -0500

    Create cluster.tf

---
+++ cluster.tf
@@ -0,0 +1,20 @@
+data "google_service_account_access_token" "my_kubernetes_sa" {
+  target_service_account = "{{service_account}}"
+  scopes                 = ["userinfo-email", "cloud-platform"]
+  lifetime               = "3600s"
+}
+
+data "google_container_cluster" "my_cluster" {
+  name     = "my-cluster"
+  location = "us-central1"
+}
+
+provider "kubernetes" {
+  load_config_file = false
+
+  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token = data.google_service_account_access_token.my_kubernetes_sa.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+  )
+}
