55ed08cec305677f659fa0fa6ba313a182b1fae5
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-09-02 16:18:11 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
   credentials = file("account.json")
-  project     = "my-project-id"
-  region      = "us-central1"
+  project     = "${var.google_project_id}"
+  region      = "${var.region}"
 }
