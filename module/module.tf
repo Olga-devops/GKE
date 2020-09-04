6c2c83ab396df96589876b3ff2e9563d57dfb003
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-09-04 12:45:43 -0500

    Update module.tf

--- module/module.tf
+++ module/module.tf
@@ -5,5 +5,5 @@ module "gke_cluster" {
     google_project_id = "Olga-project"
     cluster_node_count = "2"
     cluster_version = "1.15"
-    google_credentials = "./service-account.json" # service account 
+    google_credentials = "./service-account.json" # service account for credentials
 }
