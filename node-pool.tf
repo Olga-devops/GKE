984232fb5dbc65958ee034382dbe3a2eb890e23c
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-09-09 20:20:58 -0500

    Update node-pool.tf

--- node-pool.tf
+++ node-pool.tf
@@ -3,8 +3,8 @@ resource "google_container_cluster" "primary" {
   location = "us-central1"
 
   # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
+  # separately managed node pools. So we create the smallest possible default node pool and immediately delete it.
+  
   remove_default_node_pool = true
   initial_node_count       = 1
 
