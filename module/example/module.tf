4e6e0cc4a83b8ddca25b22ee245c7e44c91aca45
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-09-11 20:51:58 -0500

    Update module.tf

--- module/example/module.tf
+++ module/example/module.tf
@@ -1,8 +1,8 @@
 module "gke_cluster" {
     source  = "../../"
-    cluster_name = "Test-cluster"
+    cluster_name = "gke-cluster"
     google_region = "us-central1"
-    google_project_id = "Olga-project"
+    google_project_id = "caramel-anvil-289323"
     cluster_node_count = "2"
     cluster_version = "1.15"
     google_credentials = "./service-account.json" # service account for credentials
