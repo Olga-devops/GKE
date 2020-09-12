4ea576169fc5539bae58a0eae7f56ea697e83ed1
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-09-12 11:25:45 -0500

    Update module.tf

--- module/example/module.tf
+++ module/example/module.tf
@@ -1,7 +1,7 @@
 module "gke_cluster" {
     source  = "../../"
     cluster_name = "gke-cluster"
-    google_region = "us-central1"
+    region = "us-central1"
     google_project_id = "caramel-anvil-289323"
     cluster_node_count = "2"
     cluster_version = "1.15"
