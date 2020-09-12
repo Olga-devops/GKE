f233297921cba1760079ed61f5de31c782bed20e
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-09-11 20:50:32 -0500

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -1,5 +1,5 @@
 data "google_container_engine_versions" "cluster_version" {
-  location       = "${var.google_region}"
+  location       = "${var.region}"
   version_prefix = "${var.cluster_version}"
   project        = "${var.google_project_id}"
 }
@@ -11,7 +11,7 @@ resource "google_container_cluster" "create" {
   name               = "${var.cluster_name}"
   network            = "${var.cluster_network}"
   subnetwork         = "${var.subnetwork}"
-  location           = "${var.google_region}"
+  location           = "${var.region}"
   min_master_version = "${data.google_container_engine_versions.cluster_version.latest_node_version}"
   initial_node_count = "${var.cluster_node_count}"
   project            = "${var.google_project_id}"
