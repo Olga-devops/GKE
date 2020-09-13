6580a84854893d8c21120b6d20807cf752b7f84b
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-09-12 21:31:19 -0500

    Update node-pool.tf

--- node-pool.tf
+++ node-pool.tf
@@ -1,24 +1,8 @@
-#resource "google_container_cluster" "primary" {
-# name     = "my-gke-cluster"
-# location = "us-central1"
-#  # We can't create a cluster with no node pool defined, but we want to only use
-#  # separately managed node pools. So we create the smallest possible default node pool and immediately delete it. 
-# remove_default_node_pool = true
-# initial_node_count       = 1
-#
-# master_auth {
-#   username = ""
-#   password = ""
-#   client_certificate_config {
-#     issue_client_certificate = false
-#   }
-# }
-#}
-#resource "google_container_node_pool" "primary_preemptible_nodes" {
+#resource "google_container_node_pool" "nodes" {
 #  name       = "my-node-pool"
 #  location   = "us-central1"
-#  cluster    = google_container_cluster.primary.name
-#  node_count = 1
+#  cluster    = google_container_cluster.gke_cluster_olga.name
+#  node_count = 2
 #
 #  node_config {
 #    preemptible  = true
